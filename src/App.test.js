f49eb44cb8d9ca11e6909b0619dfc801ae590903
SteveKSV <amigo.kl08@gmail.com>
2024-06-18 21:02:51 +0300

    added new tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+
 test('renders learn react link', () => {
-  expect(true).toBe(true);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the logo', () => {
+  render(<App />);
+  const logo = screen.getByAltText('logo');
+  expect(logo).toBeInTheDocument();
+});
+
+test('has a link to the React website', () => {
+  render(<App />);
+  const link = screen.getByRole('link', { name: /learn react/i });
+  expect(link).toHaveAttribute('href', 'https://reactjs.org');
 });
